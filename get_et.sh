4df81d09cc4e3530292bd47ae43da35c990a0c7e
Neil Cherry <linuxha@linuxha.com>
2022-04-28 14:34:49 -0400

    Added sample comment
    Added sample comment

--- get_et.sh
+++ get_et.sh
@@ -4,3 +4,5 @@ DIR=$(dirname "$0")
 echo Calculating values. Logging to /var/log/mh/scripts/WeatherCustom.log
 
 python $DIR/weatherCustom.py $DIR/logs $DIR/ET $DIR/wuData $DIR/weatherprograms >> /var/log/mh/scripts/weatherCustom.log
+
+# comment
